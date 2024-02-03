4832de1e8de8bcb17d0d75869317cd79112a98cd
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-02-03 23:42:17 +0000

    add delete buttons on tasks page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+
   end
 
   def show
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,29 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const checkboxes = document.querySelectorAll('.task-checkbox');
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', () => {
+    const taskTitle = checkbox.closest('.container').querySelector('p');
+
+    if (checkbox.checked) {
+      taskTitle.style.textDecoration = 'line-through';
+    } else {
+      taskTitle.style.textDecoration = 'none';
+    }
+  });
+});
+
+// const binButtons = document.querySelectorAll('.fa-trash')
+
+// binButtons.forEach((button) => {
+//   button.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     const elementToRemove = event.target.closest('.fa-solid');
+//     if (elementToRemove) {
+//       elementToRemove.remove();
+//     }
+//   });
+// });
